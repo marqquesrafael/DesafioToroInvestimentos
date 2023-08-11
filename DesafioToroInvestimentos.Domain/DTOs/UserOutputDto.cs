4af40a9399a929ce068c731372a50e4e652d685d
marqquesrafael <marqquesrafael@gmail.com>
2023-08-11 09:37:05 -0300

    model user output

--- DesafioToroInvestimentos.Domain/DTOs/UserOutputDto.cs
+++ DesafioToroInvestimentos.Domain/DTOs/UserOutputDto.cs
@@ -4,13 +4,32 @@ namespace DesafioToroInvestimentos.Domain.DTOs
 {
     public class UserOutputDto
     {
+        public long Id { get; set; }
+
         public string Email { get; set; }
 
         public string FullName { get; set; }
 
         public decimal BankBalance { get; set; }
 
+        public decimal Amount { get{ return GetAmount(InvestimentAssets); } 
+        }
+
         public IEnumerable<InvestmentAssetDto> InvestimentAssets { get; set; }
+
+        private decimal GetAmount(IEnumerable<InvestmentAssetDto> investmentAsset)
+        {
+            decimal amount = 0.0M;
+
+            foreach (var item in investmentAsset)
+            {
+                var value = item.Quantity * item.Value;
+                amount += value;
+            }
+
+            return amount;
+        }
+
     }
 
     public class InvestmentAssetDto
@@ -20,8 +39,6 @@ namespace DesafioToroInvestimentos.Domain.DTOs
         public int Quantity { get; set; }
 
         public decimal Value { get; set; }
-
-        public long UserId { get; set; }
     }
 
 }
